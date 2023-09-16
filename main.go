0e18205630c26bf645d4aad3d985faec3245d98f
Enya Santos <enyalgs@gmail.com>
2023-09-16 16:24:23 -0300

    feat: add router subpackage

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/enyasantos/project-manager/router"
+
+func main() {
+	router.Initialize()
+
+}
---
+++ router/router.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Initialize() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
